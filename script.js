02fbcb7bfa608dd59be6970c1019e5477ede2105
Ivan Zadonskiy <88443911+m1dr0@users.noreply.github.com>
2023-10-28 16:05:39 +0300

    cringe

---
+++ script.js
@@ -0,0 +1,8 @@
+const email = document.getElementById("email-form")
+const submit = document.getElementById("submit-form")
+
+
+submit.addEventListener("click", function() {
+    const emailForm = email.value 
+    alert('Спасибо')
+})
